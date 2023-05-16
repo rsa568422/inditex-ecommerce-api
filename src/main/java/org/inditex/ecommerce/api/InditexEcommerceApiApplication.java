3ceb5e285c39706335dc2a726e0785a98d761186
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-05-16 20:47:25 +0200

    Initial commit

---
+++ src/main/java/org/inditex/ecommerce/api/InditexEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package org.inditex.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexEcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexEcommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/inditex/ecommerce/api/InditexEcommerceApiApplicationTests.java
@@ -0,0 +1,14 @@
+package org.inditex.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InditexEcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
