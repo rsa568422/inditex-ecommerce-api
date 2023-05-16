020a466c570b27a491cf6374c286baeb8a85620e
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-05-16 20:59:47 +0200

    Se agrega test para verificar que disponemos de los ficheros csv

--- src/test/java/org/inditex/ecommerce/api/InditexEcommerceApiApplicationTests.java
+++ src/test/java/org/inditex/ecommerce/api/InditexEcommerceApiApplicationTests.java
@@ -1,14 +1,31 @@
 package org.inditex.ecommerce.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class InditexEcommerceApiApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static final String RESOURCES = "\\src\\main\\resources\\";
 
+
+	private static final String ROOT = FileSystems.getDefault()
+			.getPath("")
+			.normalize()
+			.toAbsolutePath()
+			.toString()
+			.concat(RESOURCES);
+
+	@ParameterizedTest(name = "contextLoads for file {argumentsWithNames}")
+	@ValueSource(strings = {"product", "size-1", "stock"})
+	void contextLoads(String param) {
+		assertTrue(Path.of(ROOT.concat(String.format("%s.csv", param))).toFile().canRead());
 	}
 
 }
